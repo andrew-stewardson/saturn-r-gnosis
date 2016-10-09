fbac151d1e0f607ca2a357fae3ad891b55ed5855
Andrew Stewardson <andrew.stew@rdson.net>
2016-10-09 20:50:08 +1100

    Removed dataframe creation (separate script) and updates of models.

--- R/msm.R
+++ R/msm.R
@@ -3,79 +3,15 @@
 
 # AUTHOR: A. Stewardson
 # DATE STARTED: 23.03.2015
-# DATE UPDATED: 
+# DATE UPDATED: 09.10.2016
+
+### Set-up ----------------------------------------------------------------------
 
 require(dplyr)
 require(lubridate)
 require(msm)
 
-### 1. Make dataframe ----------------------------------------------------------------------
-
-# All patients
-# Control, Ciprofloxacin, Nitrofurantoin
-# Remove paticipants with missing dates (for now)
-
-# Run script to 'master' dataset
-rm(master)
-# csv file made using:
-#     1. source("/Users/andrewstewardson/Dropbox (HumanLabZ)/SATURN/saturnmicro/R/make_master.R")
-#     2. write.csv(master, file='master.csv')
-#     3. copy to correct folder
-
-master <- read.csv("data/master.csv")
-
-# Keep confirmed ciprofloxacin results for index patients (control, nitrofurantoin, or ciprofloxacin)
-temp <- master %>%
-  filter(test=="cip200" & (house.exposure=="control" | house.exposure=="nitrofurantoin" | house.exposure=="ciprofloxacin")) %>%
-  mutate(state=confirm.bi) %>%
-  select(charID, state, exposure, time, collection.dt)
-
-# Define day zero for each subject (=day sample 1 collected)
-day.zero <- temp %>%
-  arrange(charID, time) %>%
-  filter(time=="TP1") %>%
-  select(charID, day.zero=collection.dt)
-
-# Create variable for time from day zero for each sample
-df <- temp %>%
-  left_join(day.zero, by="charID") %>%
-  mutate(
-    int=interval(day.zero, collection.dt),  # interval between recruitment date and collection date
-    t=int/ddays(1),
-    state=state+1)
-
-# Tidy
-rm(temp, day.zero)
-
-# Define and 'factorise' exposures
-df$ab[df$exposure=="no.antibiotic"]<-"no.antibiotic"
-df$ab[df$exposure=="ciprofloxacin" & df$time=="TP1"]<-"ciprofloxacin"
-df$ab[df$exposure=="ciprofloxacin" & df$time=="TP2"]<-"post.ciprofloxacin"
-df$ab[df$exposure=="ciprofloxacin" & df$time=="TP3"]<-"post.ciprofloxacin"
-df$ab[df$exposure=="nitrofurantoin" & df$time=="TP1"]<-"nitrofurantoin"
-df$ab[df$exposure=="nitrofurantoin" & df$time=="TP2"]<-"post.nitrofurantoin"
-df$ab[df$exposure=="nitrofurantoin" & df$time=="TP3"]<-"post.nitrofurantoin"
-
-df$ab <- factor(df$ab, levels=c("no.antibiotic", "nitrofurantoin", "post.nitrofurantoin", "ciprofloxacin", "post.ciprofloxacin"))
-df$exposure <- factor(df$exposure, levels=c("no.antibiotic", "nitrofurantoin", "ciprofloxacin"))
-
-# Tidy
-df <- df %>%
-  select(-int) %>%                      # remove interval variable
-  filter(!is.na(state) & !is.na(t)) %>% # remove if state or time point is missing
-  filter(t>=0)                          # !Note: check if need to fix errors in dates!
-
-# Convert time (t) to integers then display
-df$t <- floor(df$t)
-hist(df$t)
-
-# Keep if >1 observation
-count <- df %>% group_by(charID) %>% summarise(n_obs=n()) # count observations per subject
-
-df <- df %>%
-  left_join(count, by="charID") %>%       # join number of observations to df
-  filter(n_obs>1) %>%                     # keep subjects with >1 observation
-  select(charID, t, state, ab, exposure)  # select relevant fields
+load("data/df.Rda") 
 
 ### Model 1. Specification & inits ----------------------------------------------------------------------
 
@@ -91,64 +27,76 @@ statetable.msm(state, charID, data=df.cip)
 statetable.msm(state, charID, data=df.nit)
 statetable.msm(state, charID, data=df.cnt)
 
-# Initial values
-crudeinits.msm(state ~ t, charID, data=df,
-               qmatrix=twoway2.q)
+rm(df.cip, df.cnt, df.nit)
 
-# Specifying model
-twoway2.q <- rbind(c(-0.004373006,0.004373006), c(0.020981087,--0.020981087))
+# Permitted transitions
+Q <- rbind(c(0.5, 0.5),
+           c(0.5,0.5))
+
+# Generate initial values
+Q.crude <- crudeinits.msm(state ~ t, 
+                          subject = charID, 
+                          data = df,
+                          qmatrix = Q)
 
 ### Model 1. Run ----------------------------------------------------------------------
 
-cip.msm <- msm(state ~ t, subject=charID,
-               data=df, qmatrix=twoway2.q)
+## Model 1A. Two states, no covariates
+
+# Fit model
+cip.msm <- msm(state ~ t, subject = charID, data = df, 
+               obstype=1, qmatrix = Q.crude)
+
+# Show results
 cip.msm
-plot(cip.msm)
+plot(cip.msm)   # This command which worked in Brisbane now returns errors
+plot.prevalence.msm(cip.msm, mintime=0, maxtime=60,
+                    legend.pos=c(1, 100))
 
-cip.msm_cov <- msm(state ~ t, subject=charID,
-                   data=df, qmatrix=twoway2.q,
-                   obstype=1,             # to say this is a snapshot, not exact transition time
+## Model 1B. Two states, covariates = antibiotic exposure
+
+# Fit model
+cip.msm_cov <- msm(state ~ t, subject=charID, data=df,
+                   obstype=1, qmatrix=Q.crude,           
                    covariates = ~ exposure)
+
+
+# Show results
 cip.msm_cov
-plot(cip.msm_cov)
-
-plot.prevalence.msm(cip.msm_cov, mintime = 0, maxtime = 50, legend.pos=c(5,70))
-
+plot(cip.msm_cov) # This command which worked in Brisbane now returns errors +++
+plot.prevalence.msm(cip.msm_cov, mintime = 0, maxtime = 10, legend.pos=c(5,70))
 plot.prevalence.msm(cip.msm_cov, mintime = 0, maxtime = 30, legend.pos=c(5,70),
                     covariates=list(exposure="ciprofloxacin"))
-
 plot.prevalence.msm(cip.msm_cov, mintime = 0, maxtime = 50, legend.pos=NULL,
                     subset=list(ab="ciprofloxacin"))
 
 
-### Model 1. Extract information ---------------------------------------------------------------------------------
+### Model 1B. Extract information ---------------------------------------------------------------------------------
 
 # Intensity matrices
 # = a transition intensity matrix and its confidence intervals for a given set of covariate values
 qmatrix.msm(cip.msm_cov)
-qmatrix.msm(cip.msm_cov, covariates=list(ab="no.antibiotic"))
-qmatrix.msm(cip.msm_cov, covariates=list(ab="ciprofloxacin"))
-qmatrix.msm(cip.msm_cov, covariates=list(ab="post.ciprofloxacin"))
-qmatrix.msm(cip.msm_cov, covariates=list(ab="nitrofurantoin"))
-qmatrix.msm(cip.msm_cov, covariates=list(ab="post.nitrofurantoin"))
+qmatrix.msm(cip.msm_cov, covariates=list(exposure="no.antibiotic"))
+qmatrix.msm(cip.msm_cov, covariates=list(exposure="ciprofloxacin"))
+qmatrix.msm(cip.msm_cov, covariates=list(exposure="nitrofurantoin"))
 
 # Transition probability matrix
 # = estimated transition probability matrix P(t) within a given time
-pmatrix.msm(cip.msm_cov, t = 5, covariates=list(ab="ciprofloxacin"))
+pmatrix.msm(cip.msm_cov, t = 5, covariates=list(exposure="ciprofloxacin"))
+pmatrix.msm(cip.msm_cov, t = 14, covariates=list(exposure="ciprofloxacin"))
 
 # Mean sojourn time
 # = average period in a single stay in a state
-sojourn.msm(cip.msm_cov, covariates=list(ab="no.antibiotic"))
-sojourn.msm(cip.msm_cov, covariates=list(ab="ciprofloxacin"))
-sojourn.msm(cip.msm_cov, covariates=list(ab="post.ciprofloxacin"))
-sojourn.msm(cip.msm_cov, covariates=list(ab="nitrofurantoin"))
-sojourn.msm(cip.msm_cov, covariates=list(ab="post.nitrofurantoin"))
+sojourn.msm(cip.msm_cov, covariates=list(exposure="no.antibiotic"))
+sojourn.msm(cip.msm_cov, covariates=list(exposure="nitrofurantoin"))
+sojourn.msm(cip.msm_cov, covariates=list(exposure="ciprofloxacin"))
 
 # Ratio of transition intensities
 # = estimates a ratio of two entries of the transition intensity matrix at a given set of covariate values
 qratio.msm(cip.msm_cov, ind1 = c(1,2), ind2 = c(2,1))
-qratio.msm(cip.msm_cov, ind1 = c(1,2), ind2 = c(2,1), covariates=list(ab="no.antibiotic"))
-qratio.msm(cip.msm_cov, ind1 = c(1,2), ind2 = c(2,1), covariates=list(ab="ciprofloxacin"))
+qratio.msm(cip.msm_cov, ind1 = c(1,2), ind2 = c(2,1), covariates=list(exposure="no.antibiotic"))
+qratio.msm(cip.msm_cov, ind1 = c(1,2), ind2 = c(2,1), covariates=list(exposure="nitrofurantoin"))
+qratio.msm(cip.msm_cov, ind1 = c(1,2), ind2 = c(2,1), covariates=list(exposure="ciprofloxacin"))
 
 # Hazard ratios for transition
 # =  estimated hazard ratios corresponding to each covariate effect on the transition intensities
